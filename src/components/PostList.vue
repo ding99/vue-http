e372bf16fa347fe896c0ed58b683711e6971b138
Wei Ding <ding_web@yahoo.co.jp>
2022-04-26 01:45:12 -0700

    add post list file

---
+++ src/components/PostList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button>Load Posts</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PistList'
+    }
+</script>
+
+<style scoped>
+
+</style>
