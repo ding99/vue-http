d267de0ca9b3fcf4583ea3f6deea4f2cb56bebbc
Wei Ding <ding_web@yahoo.co.jp>
2022-04-26 02:17:26 -0700

    display data

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <button @click="getPosts">Load Posts</button>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{post.id}}. {{post.title}}</h3>
+            <p>{{post.body}}</p>
+        </div>
     </div>
 </template>
 
@@ -18,6 +22,7 @@
                 axios.get ('https://jsonplaceholder.typicode.com/posts')
                 .then ((response) => {
                     console.log (response.data);
+                    this.posts = response.data;
                 })
                 .catch ((error) => {
                     console.log (error);
