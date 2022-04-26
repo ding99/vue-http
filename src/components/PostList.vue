2486b890ac80875ab844f6cc93dc918e0826f819
Wei Ding <ding_web@yahoo.co.jp>
2022-04-26 02:12:01 -0700

    fetch data

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        <button>Load Posts</button>
+        <button @click="getPosts">Load Posts</button>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        name: 'PistList'
+        name: 'PostList',
+        data () {
+            return {
+                posts: [],
+            }
+        },
+        methods: {
+            getPosts () {
+                axios.get ('https://jsonplaceholder.typicode.com/posts')
+                .then ((response) => {
+                    console.log (response.data);
+                })
+                .catch ((error) => {
+                    console.log (error);
+                })
+            }
+        }
     }
 </script>
 
